07c473f1ad2c08a343dfbd369680666775c37e62
AHMED <AHMED@MSI>
2021-12-09 21:44:15 +0200

    Add project files.

---
+++ assignment 2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter the amount you want to convert from liters to gallons: ");
+int liters = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the amount you want to convert from gallons to liter");
+int gallons = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Gallons = {liters * 1 / 3.78}");
+Console.WriteLine($"liters = {gallons * 3.78 / 1}");
